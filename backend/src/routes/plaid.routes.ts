73c74aec26eeece9334160da5eee01b6fa8b466f
forrest-herman <forrestherman13@gmail.com>
2025-11-17 22:05:13 -0500

    Add webhook route for plaid to respond

--- backend/src/routes/plaid.routes.ts
+++ backend/src/routes/plaid.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { connectPlaid, createLinkToken, syncTransactions } from "../control/plaid.controller";
+import { connectPlaid, createLinkToken, handleWebhook, syncTransactions } from "../control/plaid.controller";
 
 const router = express.Router();
 
 router.post("/create_link_token", authMiddleware, createLinkToken);
 router.post("/exchange_public_token", authMiddleware, connectPlaid);
 router.post("/transactions/sync", authMiddleware, syncTransactions);
+
+router.post("/webhook", handleWebhook);
 
 export default router;
