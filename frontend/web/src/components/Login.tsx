052c7ee985668c2f4a48743e303b5f97d632cf9b
forrest-herman <forrestherman13@gmail.com>
2025-11-17 22:08:29 -0500

    Add sign up option and automatic navigation

--- frontend/web/src/components/Login.tsx
+++ frontend/web/src/components/Login.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import supabase from "../utils/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
     const handleSignIn = async () => {
         const { data, error } = await supabase.auth.signInWithPassword({
@@ -16,6 +19,22 @@ export const Login = () => {
         }
 
         alert("Login successful!");
+        navigate("/");
+        console.log("Supabase user ID:", data.user?.id);
+    };
+
+    const handleSignUp = async () => {
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+        });
+        if (error) {
+            alert(error.message);
+            return;
+        }
+
+        alert("Sign up successful!");
+        navigate("/");
         console.log("Supabase user ID:", data.user?.id);
     };
 
@@ -24,6 +43,8 @@ export const Login = () => {
             <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
             <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' type='password' />
             <button onClick={handleSignIn}>Log In</button>
+            <p>No account?</p>
+            <button onClick={handleSignUp}>Register</button>
         </div>
     );
 };
