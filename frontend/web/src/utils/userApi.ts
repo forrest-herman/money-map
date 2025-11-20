41d3e7902ecf64c30bfe2d2519db5192ea5cdf96
forrest-herman <forrestherman13@gmail.com>
2025-11-19 22:13:43 -0500

    Add typescript types for objects

--- frontend/web/src/utils/userApi.ts
+++ frontend/web/src/utils/userApi.ts
@@ -1,12 +1,14 @@
+import type { Institution } from "@shared/types/institution.types";
 import { backendApi } from "./backendApi";
+import type { Account } from "@shared/types/account.types";
 
 export const userApi = backendApi.injectEndpoints({
     endpoints: (builder) => ({
-        getInstitutions: builder.query<any[], void>({
+        getInstitutions: builder.query<Institution[], void>({
             query: () => "/user/institutions",
             providesTags: ["Institution"],
         }),
-        getAccounts: builder.query<any[], void>({
+        getAccounts: builder.query<Account[], void>({
             query: () => "/user/accounts",
             providesTags: ["Account"],
         }),
